62823ed7290a7763202ddfa0c989bc7b40e89f99
alexsam86 <sa.alayoubi@duocuc.com>
2024-12-04 15:36:42 -0300

    autoCommit

--- apps/usuarios/serializers/estudiante_serializer.py
+++ apps/usuarios/serializers/estudiante_serializer.py
@@ -5,7 +5,7 @@ class EstudianteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Estudiante
         fields = [
-            'id', 'nombres', 'apellidos', 'rut', 'region', 'comuna', 'carrera',
+            'id', 'nombres', 'apellidos', 'rut', 'region', 'comuna', 'carrera','usuario',
             'fecha_nacimiento', 'genero', 'direccion', 'telefono', 'foto', 'cv', 'habilidades'
         ]
         depth = 1  # para incluir los detalles de region y comuna
